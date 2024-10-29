916cbc0c0ecc785b36dbe93553e0e97684404707
Augusto Robles <augusto_robles@itrsa.com.ar>
2024-10-29 13:13:45 -0300

    Crud generos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers();
 
