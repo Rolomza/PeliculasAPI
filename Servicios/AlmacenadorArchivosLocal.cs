7e776709ed9340bcd167c37a464aac3c30bb11c7
Augusto Robles <augusto_robles@itrsa.com.ar>
2024-10-31 16:47:20 -0300

    Servicio para guardar imagenes en wwwroot

---
+++ Servicios/AlmacenadorArchivosLocal.cs
@@ -0,0 +1,30 @@
+﻿
+namespace PeliculasAPI.Servicios
+{
+    public class AlmacenadorArchivosLocal : IAlmacenadorArchivos
+    {
+        private readonly IWebHostEnvironment env;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public AlmacenadorArchivosLocal(IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
+        {
+            this.env = env;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task BorrarArchivo(string ruta, string contenedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> EditarArchivo(byte[] contenido, string extension, string contenedor, string ruta, string contentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
